c5fe6178462279a506775b93cba3cb5f78447b42
funCrash <funcrash@ikmail.com>
2024-09-09 11:58:05 +0200

    Implement auto-updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,6 @@
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 use tauri_plugin_sql::{Migration, MigrationKind};
+use tauri_plugin_updater::UpdaterExt;
 
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -31,12 +32,42 @@ pub fn run() {
                 FOREIGN KEY (FK_student) REFERENCES student(PK_id)
               );",
             kind: MigrationKind::Up,
-        }
+        },
     ];
     tauri::Builder::default()
-        .plugin(tauri_plugin_sql::Builder::new().add_migrations("sqlite:database.sqlite", migrations).build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
+        .setup(|app| {
+          let handle = app.handle().clone();
+          tauri::async_runtime::spawn(async move {
+            let _ = update(handle).await;
+          });
+          Ok(())
+        })
+        .plugin(
+            tauri_plugin_sql::Builder::new()
+                .add_migrations("sqlite:database.sqlite", migrations)
+                .build(),
+        )
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+async fn update(app: tauri::AppHandle) -> Result<(), tauri_plugin_updater::Error> {
+  if let Some(update) = app.updater()?.check().await? {
+    let mut downloaded = 0;
+
+    // alternatively we could also call update.download() and update.install() separately
+    update.download_and_install(|chunk_length, content_length| {
+      downloaded += chunk_length;
+      println!("downloaded {downloaded} from {content_length:?}");
+    }, || {
+      println!("download finished");
+    }).await?;
+
+    println!("update installed");
+    app.restart();
+  }
+
+  Ok(())
 }
